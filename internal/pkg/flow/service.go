6fca4be290b26e84369ccaa03995c9a88f06fab5
biosvos <biosvos@gmail.com>
2025-08-30 19:54:55 +0900

    service 추가

---
+++ internal/pkg/flow/service.go
@@ -0,0 +1,39 @@
+package flow
+
+type Service struct {
+}
+
+func NewService() *Service {
+	return &Service{}
+}
+
+func (s *Service) Initialize() {
+
+}
+
+func (s *Service) Deinitialize() {
+
+}
+
+func (s *Service) Create() {
+
+}
+
+func (s *Service) Delete() {
+
+}
+
+func (s *Service) List() {
+}
+
+func (s *Service) Start() {
+
+}
+
+func (s *Service) Stop() {
+
+}
+
+func (s *Service) View() {
+
+}
